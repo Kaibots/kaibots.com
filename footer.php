16469a54dc0049b861a5ed64ce1847d0fe5af83a
Carlos Flores Mori <yasu@Carloss-MacBook-Pro.local>
2017-01-08 16:53:11 -0800

    Deleted everything, migrating to new page style

--- footer.php
+++
@@ -1,8 +0,0 @@
-<div class="foot site-footer">
-<hr class="thick">
-<script type="text/javascript" src="js/jquery-1.10.1.min.js"></script>
- <h4 class="footertext" style="line-height:30px">&copy; Copyright 2014 Kaibots company</h4>
- <h4 class="footertext">Email: contacto@kaibots.com</h4><br>
-</div>
- </body>
- </html>
--- lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-try{
-	if(strlen($lang)!=5){throw new Exception();}
-	$dict=json_decode(file_get_contents("lang/".$lang.".json"),true);
-}catch(Exception $e){
-	$dict=json_decode(file_get_contents("lang/es_LA.json"),true);
-}
-?>
